f971d0be17c4553d92b15be11d94ace82f65914d
Ofor-David <davidoforincloud@gmail.com>
2025-04-30 17:12:12 +0100

    pages integeration

--- src/com/daveincloud/Main.java
+++ src/com/daveincloud/Main.java
@@ -9,37 +9,49 @@ import java.util.Scanner;
 //int = 2B
 public class Main {
     public static void main(String[] args){
+        //create scanner and choice ints
         Scanner scanner = new Scanner(System.in);
         int choice;
+
+
         while(true) {
+            //first screen
             System.out.println("Choose an app to test out!");
+            //add more apps here
             System.out.println("1. Bank Simulation App");
-            choice = Integer.parseInt(scanner.nextLine());
-            if (choice == 1) {
-                int subChoice;
-                AccountManager accountManager = new AccountManager();
-                while(true){
-                System.out.println("Welcome to the account Management Dashboard!!");
-                System.out.println("Click 1 to List all accounts");
-                System.out.println("Click 2 to Create a new account");
-                System.out.println("Your Choice: ");
-                subChoice = Integer.parseInt(scanner.nextLine());
+            //  handle exceptions
+            if (!scanner.hasNextInt()){
+                System.out.println("Numbers only!!");
+            }else {
+                choice = Integer.parseInt(scanner.nextLine());
+                //enter second screen and create screen sub choice
+                if (choice == 1) {
+                    while (true) {
+                        int subChoice;
+                        AccountManager accountManager = new AccountManager();
+                        System.out.println("Welcome to the account Management Dashboard!!");
+                        System.out.println("Click 1 to Create a new account");
+                        System.out.println("Click 2 to List all accounts");
+                        System.out.println("Your Choice: ");
+                        subChoice = Integer.parseInt(scanner.nextLine());
 
-                    switch (subChoice){
-                        case 1:
-                            accountManager.listAccounts();
-                            break;
-                        case 2:
-                            System.out.println("Enter new account name: ");
-                            accountManager.createAccount(scanner.nextLine());
-                            break;
+                        switch (subChoice) {
+                            case 1:
+                                System.out.println("Enter an account name");
+                                accountManager.createAccount(scanner.nextLine());
+                                break;
+                            case 2:
+                                accountManager.listAccounts();
+                                break;
+                            default:
+                                System.out.println("Enter a valid option...");
+                        }
                     }
-
+                } else {
+                    System.out.println("invalid option, try again");
                 }
             }
+
         }
-
-
-
     }
 }
